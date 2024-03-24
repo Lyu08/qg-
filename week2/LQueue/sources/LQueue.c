7ba1423c57828fc713404fe0fd6700a72bf2415e
Lyu08 <156900810+Lyu08@users.noreply.github.com>
2024-03-24 22:24:53 +0800

    Add files via upload

--- week2/LQueue/sources/LQueue.c
+++ week2/LQueue/sources/LQueue.c
@@ -13,14 +13,13 @@ void InitLQueue(LQueue* Q) {
 	}
 	p->next = NULL;
 	Q->rear = p;
-	Q->front = p;
+	Q->front = Q->rear;
 	Q->length = 0;
 }
 
 void DestoryLQueue(LQueue* Q) {
 	ClearLQueue(Q);
 	free(Q->front);
-
 }
 
 Status IsEmptyLQueue(const LQueue* Q) {
@@ -28,16 +27,17 @@ Status IsEmptyLQueue(const LQueue* Q) {
 	else return FALSE;
 }
 
+//取队头元素 
 Status GetHeadLQueue(LQueue* Q, void* e,int size) {
-	if (Q->front == NULL) {
+	if (Q->front->next == NULL) {
 		printf("当前队列为空。\n");
 		return FALSE;
 	}
 	else {
-		if(Q->front->next!=NULL)
-		e = Q->front->data;
+		(Node*)e = Q->front->next->data ;
 		LPrint(e, size);
 		return TRUE;
+	
 	}
 }
 
@@ -49,16 +49,25 @@ int LengthLQueue(LQueue* Q) {
 Status EnLQueue(LQueue* Q, void* data) {
 	Node* p = (Node*)malloc(sizeof(Node));
 	if (!p)return FALSE;
+	else {
+		p->data = data;
+		p->next = NULL;
+		if (Q->front->next != NULL) {
+			Q->rear->next = p;
+		}
+		/*Q->rear->next = p;*/
 
-	p->data = (void*)malloc(Q->length);
-	p->data=data;
-	p->next = NULL;
-	Q->rear->next = p;
-	Q->rear = p;
-	Q->length++;
-	//printf("%d", Q->length);
-	return TRUE;
+		if (Q->front->next == NULL) {
+			Q->front->next = p;
+		}
+		Q->rear = p;
+		Q->length++;
+		//printf("%d", Q->length);
+		return TRUE;
+	}
+	
 }
+
 //出队操作
 Status DeLQueue(LQueue* Q) {
 	if (IsEmptyLQueue(&Q)) {
@@ -69,8 +78,11 @@ Status DeLQueue(LQueue* Q) {
 		Node* p;
 		p = Q->front->next;
 		if (p!=NULL) Q->front->next = p->next;
-		else free(Q->front);
-		if (Q->rear == p) Q->rear = Q->front;
+		else {
+			printf("队列已经为空，操作失败。\n");
+			return FALSE;
+		}
+		/*if (Q->rear == p) Q->rear = Q->front;*/
 		free(p);
 		Q->length--;
 		return TRUE;
@@ -96,8 +108,9 @@ void ClearLQueue(LQueue* Q) {
 Status TraverseLQueue(const LQueue* Q, int size) {
 	if (IsEmptyLQueue(Q))return 0;
 	else {
-		Node* p = Q->front->next;
-		while (p != NULL) {
+		Node* p;
+		p = Q->front->next;
+		while (p->next != NULL) {
 			LPrint(p, size);
 			p = p->next;
 		}
@@ -107,7 +120,10 @@ Status TraverseLQueue(const LQueue* Q, int size) {
 
 void LPrint(void* q,int size) {
 	//根据数据类型将void*转换成相应类型的指针
-	if (size == sizeof(int)) printf("%d int", (int*)q);
-	else if (size == sizeof(char))  printf("%c char", (char*)q);
-	else if (size == sizeof(float)) printf("%f3 float", (float*)q);
+	//printf("%d\n", size);
+	if (q) {
+		if (size == sizeof(int)) printf("%d\n", *(int*)q);
+		else if (size == sizeof(char))  printf("%c\n", *(char*)q);
+		else if (size == sizeof(double)) printf("%lf\n", *(double*)q);
+	}
 }
--- week2/LQueue/sources/main.c
+++ week2/LQueue/sources/main.c
@@ -6,7 +6,7 @@
 
 LQueue Q;
 void *data,*e=NULL,* q=NULL;
-int datasize=0;
+int datasize;
 
 
 void menu() {
@@ -24,7 +24,7 @@ void menu() {
 void typechoice() {
 	char input[100]="\0";
 	int len = 0;
-	printf("i.整型 f.浮点型 c.字符型\n请选择存入数据类型>>>");
+	printf("i.整型 d.浮点型 c.字符型\n请选择存入数据类型>>>");
 	scanf("%s", input);//防止乱输
 	len = strlen(input);
 	if (len != 1) {
@@ -32,22 +32,22 @@ void typechoice() {
 		typechoice();
 	}
 	else {
-		type = input[0]  ;
+		type = input[0];
 		printf("%c\n",type);
 		if (type == 'i') {
 			data = (void*)malloc(sizeof(int));
 			datasize = sizeof(int);
 		}
-		else if (type == 'f') {
-			data = (void*)malloc(sizeof(float));
-			datasize = sizeof(float);
+		else if (type == 'd') {
+			data = (void*)malloc(sizeof(double));
+			datasize = sizeof(double);
 		}
 		else if (type == 'c') {
 			data = (void*)malloc(sizeof(char));
 			datasize = sizeof(char);
 		}
 		else {
-			printf("请输入正确数据类型选项。k\n");
+			printf("请输入正确数据类型选项。\n");
 			typechoice();
 		}
 	}
@@ -76,26 +76,24 @@ void functionchoice() {
 			if (type == 'i') {
 				e = (int*)malloc(sizeof(int));
 				while (!scanf("%d", (int*)e)) {
-					printf("请输入正确类型\n");
+					printf("请输入正确数据\n");
 					while (getchar() != '\n');
 				}
-				printf("%d", *(int*)e);
+				//LPrint(e,datasize);
 			}
 			if (type == 'c') {
 				e = (char*)malloc(sizeof(char));
 				getchar();
 				scanf("%c", &(char*)e);
-				printf("%c", (char*)e);
 			}
-			if (type == 'f') {
-				e = (float*)malloc(sizeof(float));
-				while (!scanf("%f", (float*)e)) {
-					printf("请输入正确类型\n");
+			if (type == 'd') {
+				e = (double*)malloc(sizeof(double));
+				while (!scanf("%lf", (double*)e)) {
+					printf("请输入正确数据\n");
 					while (getchar() != '\n');
 				}
-				printf("%f", *(float*)e);
 			}
-			//memcpy(data, e, datasize);
+			memcpy(data, e, datasize);
 			if (EnLQueue(&Q, data))printf("已将该数据放入队列。\n");
 			else printf("操作失败。\n");
 			system("pause");
@@ -103,7 +101,6 @@ void functionchoice() {
 			break;
 		case'2': 
 			if (DeLQueue(&Q)) printf("成功出队。\n");
-		    else printf("操作失败。\n");
 			system("pause");
 			functionchoice();
 			break;
@@ -147,5 +144,6 @@ int main() {
 	typechoice();
 	InitLQueue(&Q);
 	printf("已成功初始化队列！\n");
+	system("cls");
 	functionchoice();
 }
