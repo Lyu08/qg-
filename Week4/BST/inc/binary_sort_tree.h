1ab21362953205d0dea5ea64ad5973288c95f5c0
Lyu08 <156900810+Lyu08@users.noreply.github.com>
2024-04-07 21:16:25 +0800

    Add files via upload

---
+++ Week4/BST/inc/binary_sort_tree.h
@@ -0,0 +1,115 @@
+//
+// Created by eke_l on 2021/4/21.
+//
+
+#ifndef BINARYSORTTREE_BINARY_SORT_TREE_H
+#define BINARYSORTTREE_BINARY_SORT_TREE_H
+
+#define true 1
+#define false 0
+#define succeed 1
+#define failed 0
+#define Status int
+
+typedef int ElemType;
+
+typedef struct Node{
+    ElemType value;
+    struct Node *left, *right;
+}Node, *NodePtr;
+
+typedef struct BinarySortTree{
+    NodePtr root ;
+} BinarySortTree, *BinarySortTreePtr;
+
+/**
+ * BST initialize
+ * @param BinarySortTreePtr BST
+ * @return is complete
+ */
+Status BST_init(BinarySortTreePtr* T);
+
+/**
+ * BST insert
+ * @param BinarySortTreePtr BST
+ * @param ElemType value to insert
+ * @return is successful
+ */
+Status BST_insert(BinarySortTreePtr T, ElemType data);
+
+/**
+ * BST delete
+ * @param BinarySortTreePtr BST
+ * @param ElemType the value for Node which will be deleted
+ * @return is successful
+ */
+Status BST_delete(BinarySortTreePtr T, ElemType data);
+
+/**
+ * BST search
+ * @param BinarySortTreePtr BST
+ * @param ElemType the value to search
+ * @return is exist
+ */
+Status BST_search(BinarySortTreePtr T, ElemType data);
+
+/**
+ * BST preorder traversal without recursion
+ * @param BinarySortTreePtr BST
+ * @param (*visit) callback
+ * @return is successful
+ */
+Status BST_preorderI(BinarySortTreePtr T, void (*visit)(NodePtr e));
+
+/**
+ * BST preorder traversal with recursion
+ * @param BinarySortTreePtr BST
+ * @param (*visit) callback
+ * @return is successful
+ */
+Status BST_preorderR(BinarySortTreePtr T, void (*visit)(NodePtr e));
+
+/**
+ * BST inorder traversal without recursion
+ * @param BinarySortTreePtr BST
+ * @param (*visit) callback
+ * @return is successful
+ */
+Status BST_inorderI(BinarySortTreePtr, void (*visit)(NodePtr));
+
+/**
+ * BST inorder traversal with recursion
+ * @param BinarySortTreePtr BST
+ * @param (*visit) callback
+ * @return is successful
+ */
+Status BST_inorderR(BinarySortTreePtr T, void (*visit)(NodePtr e));
+
+/**
+ * BST preorder traversal without recursion
+ * @param BinarySortTreePtr BST
+ * @param (*visit) callback
+ * @return is successful
+ */
+Status BST_postorderI(BinarySortTreePtr, void (*visit)(NodePtr));
+
+/**
+ * BST postorder traversal with recursion
+ * @param BinarySortTreePtr BST
+ * @param (*visit) callback
+ * @return is successful
+ */
+
+Status BST_preorderR(BinarySortTreePtr T, void (*visit)(NodePtr e));
+/**
+ * BST level order traversal
+ * @param BinarySortTreePtr BST
+ * @param (*visit) callback
+ * @return is successful
+ */
+Status BST_levelOrder(BinarySortTreePtr, void (*visit)(NodePtr));
+
+void Printdata(ElemType data);
+void Back();
+void menu();
+#endif //BINARYSORTTREE_BINARY_SORT_TREE_H
